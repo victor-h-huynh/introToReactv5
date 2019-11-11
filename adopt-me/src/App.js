3ff24ca7222747b3fccbfbf1926a8f2a18261ce2
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-11 13:19:51 -0500

    Passing in Component Props

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,20 +1,26 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", { id: "something-important"}, 
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet)
-    ]
-  );
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));

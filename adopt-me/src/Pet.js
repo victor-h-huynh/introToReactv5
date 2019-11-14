bd03595efcdb2ac1b8167983bc8af58112ceafc2
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-14 14:57:08 -0500

    JSX Composite Components & Expressions

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -2,9 +2,9 @@ import React from "react";
 export default function Pet({ name, animal, breed }) {
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>Name: {name.toUpperCase()}</h1>
       <h2>{animal}</h2>
-      <h2>{breed}</h2>
+      <h2>{breed + " " + animal}</h2>
     </div>
   );
 }
